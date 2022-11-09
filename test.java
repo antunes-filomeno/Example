640f9fd10aea4b3abbf0cbf9042547d42833ca05
Gina Lopez <91145586+lopez-gina@users.noreply.github.com>
2022-11-09 14:37:42 +0100

    Update test.java

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello jenkins");
         System.out.println("Valeria was here");
+        System.out.println("Hi, Gina here!");
     }
 }
