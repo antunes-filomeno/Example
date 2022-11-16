7691a25aae6f0e88f357d3abc1a84e6110837ab1
Filomeno ANTUNES <filomeno.antunes.001@student.uni.lu>
2022-11-16 12:11:40 +0100

    commit6

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello jenkins, Filomeno here!")
+        System.out.println("Hello jenkins, Filomeno here!" + 10/0);
         System.out.println("Valeria was here");
         System.out.println("Hi, Gina here!");
     }
