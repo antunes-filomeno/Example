9145bb66d8e832da8daab9b5e17814cfb7aef68d
antunes-filomeno <91291718+antunes-filomeno@users.noreply.github.com>
2022-12-13 16:58:01 +0100

    Add files via upload

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+public class DemoApplicationTests {
+
+    private static double number1 = 15, number2 = 3;
+
+    public static void main(String[] args) {
+        System.out.println(add(number1, number2));
+        System.out.println(subtract(number1, number2));
+        System.out.println(multiply(number1, number2));
+        System.out.println(divide(number1, number2));
+    }
+
+    public static double add(double number1, double number2)
+    {
+        return number1+number2;
+    }
+
+    public static double subtract(double number1, double number2)
+    {
+        return number1-number2;
+    }
+
+    public static double multiply(double number1, double number2)
+    {
+        return number1*number2;
+    }
+
+    public static double divide(double number1, double number2)
+    {
+        return number1/number2;
+    }
+
+}
