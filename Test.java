62c9386d7bbe27b7d79e27e117714d27cbe8b19d
Filomeno ANTUNES <filomeno.antunes.001@student.uni.lu>
2022-12-13 15:56:33 +0100

    commit3

--- Test.java
+++ Test.java
@@ -6,7 +6,7 @@ public class Test {
         System.out.println(add(number1, number2));
         System.out.println(subtract(number1, number2));
         System.out.println(multiply(number1, number2));
-        System.out.println(divide(number1, number2));
+        System.out.println(number1/number2);
     }
 
     public static double add(double number1, double number2)
