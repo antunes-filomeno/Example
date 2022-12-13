6da7670e07f2a87b4087407c10246f206dabb3e9
Filomeno ANTUNES <filomeno.antunes.001@student.uni.lu>
2022-12-13 15:50:33 +0100

    commit3

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
-    private static double number1 = 15, number2 = 3;
+    private static double number1 = 15, number2 = 0;
 
     public static void main(String[] args) {
         System.out.println(add(number1, number2));
@@ -11,7 +11,7 @@ public class Test {
 
     public static double add(double number1, double number2)
     {
-        return number1+number2
+        return number1+number2;
     }
 
     public static double subtract(double number1, double number2)
